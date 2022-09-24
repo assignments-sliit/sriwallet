4f2282f21894f9f9d73ab38d2d471597dab88040
Manoj Kumar <dharmakumar338@gmail.com>
2022-09-24 05:33:28 +0530

    code format

--- lib/money/screens/receive_money.dart
+++ lib/money/screens/receive_money.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
 class ReceiveMoneyPage extends StatefulWidget {
@@ -12,18 +10,15 @@ class ReceiveMoneyPage extends StatefulWidget {
 }
 
 class _ReceiveMoneyPageState extends State<ReceiveMoneyPage> {
-FirebaseAuth auth = FirebaseAuth.instance;
-
-
+  FirebaseAuth auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( iconTheme: const IconThemeData(color: Colors.white),
-        
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.white),
         backgroundColor: Theme.of(context).primaryColor,
         title: const Text(
-
           "Receive Money",
           style: TextStyle(color: Colors.white),
         ),
